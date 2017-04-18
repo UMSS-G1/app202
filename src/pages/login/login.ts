d1dbfb756320e8de00050332c36afb802f71f3ea
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-04-18 19:39:37 -0400

    implement alerts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController, AlertController } from 'ionic-angular';
 
-/**
- * Generated class for the Login page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -14,11 +8,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadCtrl: LoadingController,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Login');
+    let load = this.loadCtrl.create({
+      content: 'estoy cargando los datos'
+    });
+    load.present();
+    setTimeout(()=>{
+      load.dismiss();
+    }, 5000);
+    load.onDidDismiss(()=>{
+      console.log("el load se ha cerrado");
+    });
+  }
+
+  showToast(){
+    let toast = this.toastCtrl.create({
+      message: 'Felicitaciones has ganado',
+      showCloseButton: true,
+      closeButtonText: 'Omitir',
+      duration: 10000,
+      position: 'middle'
+    });
+    toast.present();
+    toast.onDidDismiss(()=>{
+      console.log('el toast se ha cerrado');
+    });
+  }
+
+  showAlert(){
+    let alert = this.alertCtrl.create({
+      title: 'Cuidado!',
+      message: 'La bateria esta baja',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  showConfirm(){
+    let alert = this.alertCtrl.create({
+      title: '¿Esta seguró?',
+      message: 'se borrara permanentemente el arhivo',
+      buttons: [
+        {
+          text: 'Si, estoy seguró',
+          handler: ()=>{
+            console.log('si lo estoy');
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: ()=>{
+            console.log('cancelar');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   goToHomePage(){
